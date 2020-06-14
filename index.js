6d2b212cd25cbb2f5edb543b3298647fff0aa0cc
KelliePetersen <kelliepetersen@outlook.com>
2020-06-14 20:19:33 +1000

    set up google sign up and authentication

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
-}, (accessToken) => {
+}, (accessToken, refreshToken, profile, done) => {
   console.log(accessToken);
 }));
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
