381e28c6682a269b6cbac3707ef045f51012a237
KelliePetersen <kelliepetersen@outlook.com>
2020-06-17 13:02:44 +1000

    create fetchUser action and connect to App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 import Header from './Header';
 
 const Dashboard = () => <div>Dashboard</div>
 const SurveyNew = () => <div>SurveyNew</div>
 const Landing = () => <div>Landing</div>
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  )
+class App extends Component {
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect(null, actions)(App);
