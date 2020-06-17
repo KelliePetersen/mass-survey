4651635c947a76b8e32e9f661f8febe77aea3be6
KelliePetersen <kelliepetersen@outlook.com>
2020-06-17 14:36:15 +1000

    send logged in state to Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Log in with Google</a></li>
+      default:
+        return <li><a href="#">Logout</a></li>;
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Mass Survey</a>
+          <a href="#" className="left brand-logo">Mass Survey</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a>Log in with Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +27,13 @@ export class Header extends Component {
   }
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => ({
+  auth
+})
+
+const mapDispatchToProps = {
+  
+}
+
+
+export default connect(mapStateToProps)(Header);
